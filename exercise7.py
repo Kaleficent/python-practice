e2e0aba2e72dae3661b309f5d4e9ebb5e141d549
Kaleficent <45493208+Kaleficent@users.noreply.github.com>
2018-11-30 13:48:38 -0500

    Exercise 7

---
+++ exercise7.py
@@ -0,0 +1,23 @@
+"""
+From https://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
+
+Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+"""
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#Print a list of only even
+print(a[1::2])
+
+#Create new list of only even
+b = a[1::2]
+print(b)
+
+b.clear()
+
+#General case
+for x in a:
+	if x % 2 == 0:
+		b.append(x)
+
+print(b)
