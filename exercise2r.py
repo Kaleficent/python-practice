52e87939f97f7cd4ab5997ab64f2fa00a60e6940
C.M. Pickell <chylene.pickell@gmail.com>
2020-05-29 14:57:00 -0400

    Added additional comments

--- exercise2r.py
+++ exercise2r.py
@@ -1,14 +1,14 @@
 # Refactor of Exercise 2
-
 num = "a"
 check = "b"
 
-# Check that a whole number has been entered
+# Check that a non-zero natural number has been entered
 def valid(number):
 	while number.isdigit() == False or int(number) == 0:
 		number = input("Please enter a whole non-zero number: ")
 	return number
 
+# Check whether a number is divisible by 2 or 4
 def checkeven(number):
 	if int(number) % 2 == 0:
 		print(number + " is even.")
@@ -17,6 +17,7 @@ def checkeven(number):
 	else:
 		print(number + " is odd.")
 
+# Check if any non-zero natural number is divisible by another
 def divisible(number, divisor):
 	remainder = int(number) % int(divisor)
 	if remainder == 0:
