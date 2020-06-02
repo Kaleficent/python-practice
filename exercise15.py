2818e64473972d6d3defbafc295ef740f19323cc
Kaleficent <kaleficence@gmail.com>
2020-06-02 17:53:40 -0400

    Exercise 15

---
+++ exercise15.py
@@ -0,0 +1,22 @@
+"""
+From: https://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+
+Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+  My name is Michele
+
+Then I would see the string:
+
+  Michele is name My
+
+shown back to me.
+"""
+
+def reverse(sentence):
+  rev = sentence.split()
+  rev.reverse()
+  print("Your reversed sentence reads:")
+  print(" ".join(rev))
+
+sentence = input("This program reverses the words in a sentence. Please write a sentence.\n")
+reverse(sentence)
